976f9c94f2c8d96333c704d044f10f521619d8ed
Raphaël <r.courivaud@gmail.com>
2017-07-22 15:43:31 +0200

    add session in parse_requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 from pip.req import parse_requirements
 
-install_reqs = parse_requirements("requirements.txt")
+install_reqs = parse_requirements("requirements.txt", session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
